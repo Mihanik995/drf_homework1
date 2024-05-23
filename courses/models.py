e1f0660ecd97b46cb8b522d14dd64242b3ce07e9
user <user@example.com>
2024-05-23 14:58:32 +0300

    minor fixes

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
+NULLABLE = {'blank': True, 'null': True}
 class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    preview = models.ImageField(upload_to='courses/')
+    preview = models.ImageField(upload_to='courses/', **NULLABLE)
 
     def __str__(self):
         return self.title
@@ -13,8 +13,8 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='lessons/')
-    video_link = models.TextField()
+    image = models.ImageField(upload_to='lessons/', **NULLABLE)
+    video_link = models.TextField(**NULLABLE)
 
     course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
 
--- users/models.py
+++ users/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from courses.models import NULLABLE
+
 
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(verbose_name=_('e-mail'), unique=True)
+    email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     phone = models.IntegerField()
     city = models.CharField(max_length=50)
-    avatar = models.ImageField(upload_to='users/')
+    avatar = models.ImageField(upload_to='users/', **NULLABLE)
