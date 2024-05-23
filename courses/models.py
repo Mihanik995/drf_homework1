009f374628a188f20c7e51517a2a5dff1b11303c
user <user@example.com>
2024-05-23 14:12:37 +0300

    add entities

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    preview = models.ImageField(upload_to='courses/')
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='lessons/')
+    video_link = models.TextField()
+
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(verbose_name=_('e-mail'), unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    phone = models.IntegerField()
+    city = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to='users/')
