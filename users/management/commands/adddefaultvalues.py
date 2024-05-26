d2c7921ee73ed259552e4eb27cf0a9982c98dc51
user <user@example.com>
2024-05-26 14:51:02 +0300

    add super user and default values creation custom commands

---
+++ users/management/commands/adddefaultvalues.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from django.core.management import BaseCommand
+
+from courses.models import Lesson, Course
+from users.models import Payment, User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        default_course = Course.objects.create(
+            title='default course',
+            description='default course description'
+        )
+        default_course.save()
+
+        default_lesson = Lesson.objects.create(
+            title='default lesson',
+            description='default lesson description',
+            course=default_course
+        )
+        default_lesson.save()
+
+        Payment.objects.create(
+            summ=1000,
+            user=User.objects.get(email='admin@test.com'),
+            lesson=default_lesson
+        ).save()
+        Payment.objects.create(
+            summ=10000,
+            user=User.objects.get(email='admin@test.com'),
+            course=default_course
+        ).save()
