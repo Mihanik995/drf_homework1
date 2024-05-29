9519e905426b26df4f8302d7882b4c21c92b2c18
user <user@example.com>
2024-05-29 14:52:01 +0300

    add subscriptions to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -43,13 +43,15 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         return user
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'phone', 'city')
-
-
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = ('course',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    subscriptions = SubscriptionSerializer(source='subscription_set', many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone', 'city', 'subscriptions',)
